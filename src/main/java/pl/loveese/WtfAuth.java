7c73ed9e08770cf39079f3d18293a85e9658bbdd
loveese <poketyreklive@gmail.com>
2022-07-07 15:09:50 +0200

    Update plugin.yml and WtfAuth.java

--- src/main/java/pl/loveese/WtfAuth.java
+++ src/main/java/pl/loveese/WtfAuth.java
@@ -3,6 +3,7 @@ package pl.loveese;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import pl.loveese.cmds.ChangePasswordCommand;
+import pl.loveese.cmds.InfoCommand;
 import pl.loveese.cmds.LoginCommand;
 import pl.loveese.cmds.RegisterCommand;
 import pl.loveese.events.onJoin;
@@ -26,6 +27,7 @@ public class WtfAuth extends JavaPlugin {
         new LoginCommand(this);
         new RegisterCommand(this);
         new ChangePasswordCommand(this);
+        new InfoCommand(this);
 
         getServer().getPluginManager().registerEvents(new onJoin(), this);
         getServer().getPluginManager().registerEvents(new onMove(), this);
