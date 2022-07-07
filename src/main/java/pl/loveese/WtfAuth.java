24a2a0e8533b041160f750ee5557750324b74412
loveese <poketyreklive@gmail.com>
2022-07-07 16:29:56 +0200

    Fix / Update

--- src/main/java/pl/loveese/WtfAuth.java
+++ src/main/java/pl/loveese/WtfAuth.java
@@ -3,7 +3,6 @@ package pl.loveese;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import pl.loveese.cmds.*;
-import pl.loveese.data.SpawnJoinData;
 import pl.loveese.events.onJoin;
 import pl.loveese.events.onMove;
 import pl.loveese.events.onPlayerDropItem;
@@ -13,24 +12,20 @@ import pl.loveese.data.PlayerData;
 public class WtfAuth extends JavaPlugin {
 
     PlayerData pd;
-    SpawnJoinData sd;
 
     public WtfAuth() {
         pd = PlayerData.getInstance();
-        sd = SpawnJoinData.getInstance();
     }
 
     public void onEnable() {
         getConfig().options().copyDefaults(true);
         saveConfig();
         pd.setup(this);
-        sd.setup(this);
 
         new LoginCommand(this);
         new RegisterCommand(this);
         new ChangePasswordCommand(this);
         new InfoCommand(this);
-        new WtfAuthAdminCommand(this);
 
         getServer().getPluginManager().registerEvents(new onJoin(), this);
         getServer().getPluginManager().registerEvents(new onMove(), this);
--- src/main/java/pl/loveese/events/onJoin.java
+++ src/main/java/pl/loveese/events/onJoin.java
@@ -10,7 +10,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import pl.loveese.data.SpawnJoinData;
 
 public class onJoin implements Listener {
 
